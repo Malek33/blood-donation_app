4385571be66bcee2f8ed0613a8acec3a7faee3cb
Malek Maghraoui <malek.magraoui3@gmail.com>
2022-06-07 19:04:27 +0100

    Update connectDB.js

--- config/connectDB.js
+++ config/connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect("mongodb+srv://malek:malek@cluster0.gnfc8.mongodb.net/mySecDatabase?retryWrites=true&w=majority");
         console.log("database is connected");
     } catch (err) {
         console.log(err);
